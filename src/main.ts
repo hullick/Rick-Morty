baa728dee639b7bc8b47f7032e1e811916d38ea6
Hullick Bartholo Gomes <hullick02@gmail.com>
2021-06-05 09:37:14 -0300

    Created .env and installed Apollo

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,33 @@
 import { createApp } from "vue";
 import App from "./App.vue";
+import { ApolloClient, createHttpLink, InMemoryCache } from '@apollo/client/core'
+import { DefaultApolloClient } from "@vue/apollo-composable";
+import { createStore } from 'vuex'
+import 'es6-promise/auto'
 
+const httpLink = createHttpLink({
+    uri: process.env.VUE_APP_GRAPHQL_API,
+});
 
-createApp(App).mount("#app");
+// Cache implementation
+const cache = new InMemoryCache();
+
+// Create the apollo client
+const apolloClient = new ApolloClient({
+    link: httpLink,
+    cache,
+});
+
+// Create a new store instance.
+const store = createStore({
+    state(){
+        return {
+
+        }
+    }
+});
+
+createApp(App)
+    .provide(DefaultApolloClient, apolloClient)
+    .use(store)
+    .mount("#app");
